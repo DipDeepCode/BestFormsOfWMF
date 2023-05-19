ba6c8ca8c6708d7534b0fa786cfbd8c3632ae8e7
Sergey Kovalev <dipdeepcode@mail.ru>
2023-05-19 22:41:43 +0300

    Небольшой рефакторинг, добавлена многопоточность для ускорения

--- src/test/java/ru/ddc/AppTest.java
+++ src/test/java/ru/ddc/AppTest.java
@@ -1,20 +1,13 @@
 package ru.ddc;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test1() {
+        App.main(null);
     }
 }
